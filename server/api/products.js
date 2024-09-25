29ccd713ec11b4ed928942302964f20e4b1d65a4
Abhishek Kumar <abhi1322jii@gmail.com>
2024-09-25 19:33:53 +0530

    feat: getproducts setup

---
+++ server/api/products.js
@@ -0,0 +1,20 @@
+const getProducts = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve({
+      products: [
+        {
+          id: 1,
+          name: "Product 1",
+          price: 9.99,
+        },
+        {
+          id: 2,
+          name: "Product 2",
+          price: 19.99,
+        },
+      ],
+    });
+  }, 2000);
+});
+
+export default getProducts;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 import express from "express";
+import getProducts from "./api/products";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+});
+
+app.get("/products", async (req, res) => {
+  const products = await getProducts;
+
+  res.json({ products });
 });
 
 app.listen(3000, () => {
