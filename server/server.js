203c63ef4f6bee9fb309181c0cf6398c6ef2160f
Abhishek Kumar <abhi1322jii@gmail.com>
2024-10-01 15:08:37 +0530

    feat: first redis get & set

--- server/server.js
+++ server/server.js
@@ -1,15 +1,33 @@
 import express from "express";
 import { getProducts } from "./api/products.js";
-import { Redis } from "ioredis";
+import Redis from "ioredis";
 
 const app = express();
+const redis = new Redis({
+  host: "redis-15332.c340.ap-northeast-2-1.ec2.redns.redis-cloud.com",
+  port: 15332,
+  password: "HfydXqtxyVio4e507VIukIldFaydkTNB",
+});
+
+redis.on("connect", () => {
+  console.log("Connected to Redis");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
 app.get("/products", async (req, res) => {
+  const isExist = await redis.exists("products");
+
+  if (isExist) {
+    const cachedProducts = await redis.get("products");
+    return res.json({ products: JSON.parse(cachedProducts) });
+  }
+
   const products = await getProducts();
+
+  redis.setex("products", 60, JSON.stringify(products)); // Cache for 1 minute
 
   res.json({ products });
 });
